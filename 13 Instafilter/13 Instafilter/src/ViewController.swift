ca984f78e73dadebf265b6addaa6e4ff8ccc7e7c
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-22 15:41:22 -0300

    Permitir que o usuário escolha um filtro

--- 13 Instafilter/13 Instafilter/src/ViewController.swift
+++ 13 Instafilter/13 Instafilter/src/ViewController.swift
@@ -1,18 +1,42 @@
+import CoreImage
 import UIKit
 
 class ViewController: UIViewController {
+    let availableFilters = [
+        "CIBumpDistortion",
+        "CIGaussianBlur",
+        "CIPixellate",
+        "CISepiaTone",
+        "CITwirlDistortion",
+        "CIUnsharpMask",
+        "CIVignette",
+    ]
+
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
 
     var currentImage: UIImage!
 
+    var context: CIContext!
+    var currentFilter: CIFilter!
+
     @IBAction func changeFilter(_ sender: Any) {
+        let alertController = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+
+        availableFilters.forEach { filter in
+            alertController.addAction(UIAlertAction(title: filter, style: .default, handler: setFilter))
+        }
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(alertController, animated: true)
     }
 
     @IBAction func save(_ sender: Any) {
     }
 
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
     }
 
     override func viewDidLoad() {
@@ -24,6 +48,9 @@ class ViewController: UIViewController {
             target: self,
             action: #selector(importPicture)
         )
+
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     @objc func importPicture() {
@@ -31,6 +58,34 @@ class ViewController: UIViewController {
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
+    }
+
+    func applyProcessing() {
+        guard let image = currentFilter.outputImage else { return }
+        //currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+
+        guard let cgimg = context.createCGImage(image, from: image.extent) else { return }
+        let processedImage = UIImage(cgImage: cgimg)
+        imageView.image = processedImage
+    }
+
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else { return }
+        guard let actionTitle = action.title else { return }
+
+        currentFilter = CIFilter(name: actionTitle)
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+
+        applyProcessing()
     }
 }
 
@@ -42,5 +97,9 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 
         currentImage = image
         //imageView.image = image
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 }
