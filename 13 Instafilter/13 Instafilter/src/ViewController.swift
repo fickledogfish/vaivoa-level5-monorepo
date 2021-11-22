9404b2afcd78d5e0b6ea09e7b34a7da6f14ff71b
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-22 15:23:05 -0300

    Importar uma imagem da galeria

--- 13 Instafilter/13 Instafilter/src/ViewController.swift
+++ 13 Instafilter/13 Instafilter/src/ViewController.swift
@@ -1,7 +1,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+
+    var currentImage: UIImage!
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(importPicture)
+        )
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        dismiss(animated: true)
+
+        currentImage = image
+        //imageView.image = image
     }
 }
